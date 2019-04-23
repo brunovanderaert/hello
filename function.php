a045fa6f41e9585d9a2d71cd878f00132f981975
Bruno Vanderaert <bruno.vanderaert@gmail.com>
2019-04-23 11:58:56 +0200

    ajout d'une nouvelle fonction

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
 function getAllannonce() {
     $data =[
-        "annonce1" => ["voiture",1000]
+        "annonce1" => ["voiture",1000] ,
+        "annonce2" => ["meuble",55]
     ];
     return $data;
 }

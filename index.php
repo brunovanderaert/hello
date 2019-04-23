c8c73d3668b2a4741e56fec053f3ca02f858f933
Bruno Vanderaert <bruno.vanderaert@gmail.com>
2019-04-23 16:33:46 +0200

    rectif Jquery

--- index.php
+++ index.php
@@ -4,32 +4,46 @@ $resultat = getAllannonces();
 
 ?>
 
-<ul>
-    <li>
-        <?php
-        echo $resultat["annonce1"][0];
-        ?>
-    </li>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="jquery.js"></script>
 
-    <li>
-    <?php
-        echo $resultat["annonce1"][1];
-        ?>
-    </li>
-</ul>
-    <hr>
-<ul>
-    <li>
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <?php
+            echo $resultat["annonce1"][0];
+            ?>
+        </li>
+
+        <li>
+            <?php
+                echo $resultat["annonce1"][1];
+            ?>
+        </li>
+    </ul>
+        <hr>
+    <ul>
+        <li>
+            <?php
+                echo $resultat["annonce2"][0];
+            ?>
+        </li>
+        <li>
         <?php
-        echo $resultat["annonce2"][0];
-        ?>
-    </li>
-    <li>
-    <?php
-        echo $resultat["annonce2"][1];
-        ?>
-    </li>
-</ul>
+            echo $resultat["annonce2"][1];
+            ?>
+        </li>
+    </ul>
+    <div></div>
+</body>
+</html>
 
 <?php
 $first = getFirstannonce();
@@ -41,6 +55,6 @@ echo "<pre>";
 print_r($newProduit);
 ?>
 
-<script href='Jquery.js'>
-$('hello');
+<script>
+$('div').html("hello");
 </script>
